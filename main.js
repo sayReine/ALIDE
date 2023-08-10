af20ef8138e933f124e3197a89e1ebcb8495739a
Reine Esther <Esther>
2023-08-10 12:28:56 -1000

    new changes

---
+++ main.js
@@ -0,0 +1,18 @@
+function navigateToAnotherPage(){
+    window.location.href ='dress1.html';
+}
+function navigateToAnotherPage1(){
+    window.location.href ='dress2.html';
+}
+function navigateToAnotherPage2(){
+    window.location.href ='dress3.html';
+}
+function navigateToAnotherPage3(){
+    window.location.href ='dress4.html';
+}
+function navigateToAnotherPage4(){
+    window.location.href ='dress5.html';
+}
+function navigateToAnotherPage5(){
+    window.location.href ='dress6.html';
+}
